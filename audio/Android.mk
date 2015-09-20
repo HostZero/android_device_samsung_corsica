55d56198892cb2497accbcf7f2e8e72039562286
karthick111 <karthickrko61@gmail.com>
2015-09-20 10:02:14 +0530

    remove old files

--- audio/Android.mk
+++ audio/Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH:= $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
-  CorsicaAudioPolicyManager.cpp
+  RheaAudioPolicyManager.cpp
 
 LOCAL_SHARED_LIBRARIES := \
   libcutils \
